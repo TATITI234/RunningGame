d2d94ce43cb0e9ee6fb25d4ef7d39a41199a7487
TATITI234 <yunt933@gmail.com>
2025-09-08 16:02:12 +0900

    25.09.08 update
    별거안함

--- Assets/Scripts/AreaManager.cs
+++ Assets/Scripts/AreaManager.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 블록 종류를 ㅈㄴ 많이 만들고 블록들을 한 오브젝트의 자식으로 둔 뒤
+// 자식 오브젝트 중 하나를 랜덤으로 뽑아서 액티브 온
+// 화면에서 블록이 사라지면 
 public class AreaManager : MonoBehaviour
 {
-    public List<GameObject> Areas;
-    public Vector2 targetPos;
+    public List<GameObject> Areas;  // 블록 종류
+    public Vector2 targetPos;   // 블록이 생성될 위치
 
-    public GameObject areas1;
-    public GameObject areas2;
+    public GameObject areasPrefab;  // 모든 종류 블록의 부모 프리팹
 
-    [SerializeField] private float speed = 2;
-    public bool isMove = true;
+    [SerializeField] private float speed = 2;   // 블록 스피드
+    public bool isMove = true;  // 블록이 움직일 수 있는지 여부
 
-
+    private GameObject childObj;    // 
 
     private void Awake()
     {
@@ -21,34 +23,38 @@ public class AreaManager : MonoBehaviour
     }
     private void Start()
     {
-        GenerateArea();
     }
 
 
     void FixedUpdate()
     {
-        if (isMove)
-        {
-            transform.Translate(new Vector2(-0.1f * speed, 0));
-        }
+        MoveArea(areasPrefab);
 
     }
+
     private void Update()
     {
-        if (areas1.transform.position.x <= 0)
+        if (areasPrefab.transform.position.x <= 0)
         {
-            GenerateArea(areas2);
+            GenerateArea(areasPrefab);
         }
-        else if(areas2.transform.position.x <= 0)
-        {
-            GenerateArea(areas1);
-        }
+
 
     }
-
+    // 블록 생성
     public void GenerateArea(GameObject area)
     {
         area = Instantiate(Areas[Random.Range(0, Areas.Count)], targetPos, Quaternion.identity);
+        childObj = Areas[Random.Range(0, Areas.Count)]
+    }
 
+    // 블록 움직임
+    private void MoveArea(GameObject area)
+    {
+        if (isMove)
+        {
+            area.transform.Translate(new Vector2(-0.1f * speed, 0));
+
+        }
     }
 }
