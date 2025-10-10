aabff0d8e0b701cf4ec67c749185c9fdf127fa03
TATITI234 <yunt933@gmail.com>
2025-10-10 14:37:07 +0900

    Revert "25.10.10 update"
    This reverts commit 7bebaf6b901f354c6a6a0f5170cf1f67587528c3.

--- Assets/Scripts/GameDataManager.cs
+++ Assets/Scripts/GameDataManager.cs
@@ -12,7 +12,27 @@ public class GameDataManager : MonoBehaviour
     }
 
     public float speed = 2;   // 블록 스피드
+<<<<<<< Updated upstream
+    public GameObject Jellys;
+=======
     public float blockSize = 18;    // 블록의 크기
+>>>>>>> Stashed changes
 
+    public List<GameObject> jellyObj = new List<GameObject>();
+    private void Start()
+    {/*
+        for (int i = 0; i < Jellys.transform.childCount; i++)
+        {
+            
+            jellyObj.Add(Jellys.transform.GetChild(i).gameObject);
+        }*/
+        /* foreach 를 사용하면? */
+        foreach (Transform child in Jellys.transform)
+        {
+            // 위치값 오브젝트 배열에 저장하기
+            jellyObj.Add(child.gameObject);
+        }
+       
 
+    }
 }
